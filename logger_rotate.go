b26fc18271590fc8cd45ad282e9e725274f8b6f4
rfancn <reg_info@qq.com>
2025-05-29 17:46:31 +0800

    add namespace to rotate dir

--- logger_rotate.go
+++ logger_rotate.go
@@ -29,15 +29,15 @@ func newRotateLogger(conf *zerologProviderConfig) (io.Writer, error) {
 
 	// 创建日志目录
 	fileSuffix := path.Ext(conf.Filename)
-	filename := strings.TrimSuffix(conf.Filename, fileSuffix)
-	rotateDir := path.Join(dir, normalize(filename))
+	filename := normalize(strings.TrimSuffix(conf.Filename, fileSuffix))
+	rotateDir := path.Join(dir, filename)
 	err := os.MkdirAll(rotateDir, 0744)
 	if err != nil {
 		return nil, err
 	}
 
 	return &lumberjack.Logger{
-		Filename:   filepath.Join(rotateDir, normalize(filename)+fileSuffix),
+		Filename:   filepath.Join(rotateDir, filename+fileSuffix),
 		MaxSize:    conf.Rotate.MaxSize,   // The maximum size in megabytes of the log file before it gets rotated, It defaults to 100 megabytes.
 		MaxAge:     conf.Rotate.MaxAge,    // In days before deleting the file
 		MaxBackups: conf.Rotate.MaxBackup, // The maximum number of old log files to retain.
